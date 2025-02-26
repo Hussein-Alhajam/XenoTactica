924be074ad6599c2ed16881ceaa4c0e0c9add394
Hussein Alhajam <hussein.alhajam@ontariotechu.net>
2025-02-25 23:03:50 -0500

    removed extra battle scene

--- tactical_grid_movement/Transitions/battle_transition.gd
+++ tactical_grid_movement/Transitions/battle_transition.gd
@@ -5,4 +5,4 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_home"):
 		get_tree().change_scene_to_file("res://Overworld/over_world.tscn")
 	if Input.is_action_pressed("ui_page_down"):
-		get_tree().change_scene_to_file("res://demo_turn_based_combat_/scenes/battle_scene2.tscn")
+		get_tree().change_scene_to_file("res://demo_turn_based_combat_/scenes/battle_scene.tscn")
