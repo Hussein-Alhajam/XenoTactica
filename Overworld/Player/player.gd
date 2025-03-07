f66278bdfb47378d15e5a6c7f4abeac099fef2ac
Hussein-Alhajam <90421975+Hussein-Alhajam@users.noreply.github.com>
2025-03-07 10:47:21 -0500

    Other Player movement added for testing

--- Overworld/Player/player.gd
+++ Overworld/Player/player.gd
@@ -40,3 +40,15 @@ func save_and_transition(next_scene: String):
 		overworld.transition_to_scene(next_scene)
 	else:
 		get_tree().change_scene_to_file(next_scene)
+		
+#extends CharacterBody2D
+
+#@export var speed = 400
+
+#func get_input():
+#	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+#	velocity = input_direction * speed
+
+#func _physics_process(delta):
+#	get_input()
+#	move_and_slide()
