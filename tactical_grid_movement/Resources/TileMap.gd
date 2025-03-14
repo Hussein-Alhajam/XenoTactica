639f349709d0ad1ebb6b9d81549379d3fc319c8f
Hussein-Alhajam <90421975+Hussein-Alhajam@users.noreply.github.com>
2025-03-14 15:15:07 -0400

    Fixed dependancy issues in the overworld scene

--- tactical_grid_movement/Resources/TileMap.gd
+++ tactical_grid_movement/Resources/TileMap.gd
@@ -1,20 +1,4 @@
 extends TileMapLayer
-<<<<<<< Updated upstream
-var movement_data
-
-
-## Get the movement cost of any single cell on the map
-## We pass in the grid, so that we don't take in the data from tiles that have been placed outside the play area
-func get_movement_costs(grid: Grid):
-	var movement_costs = []
-	for y in range(grid.size.y):
-		movement_costs.append([])
-		for x in range(grid.size.x):
-			var tile = get_cell_source_id(Vector2i(x,y))
-			var movement_cost = movement_data.get(tile)
-			movement_costs[y].append(movement_cost)
-	return movement_costs
-=======
 class_name Map
 
 @export var grid_manager: GridManager
@@ -31,4 +15,3 @@ func is_tile_walkable(tile_position: Vector2i) -> bool:
 
 func mark_obstacle(tile_position: Vector2i):
 	grid_manager.astar_grid.set_point_solid(tile_position)
->>>>>>> Stashed changes
